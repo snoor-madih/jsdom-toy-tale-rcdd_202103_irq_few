808f7e1cef90a410ddcdaba027d7ba8f28db01c9
snoor_madih <snoor.madih@gmail.com>
2021-06-04 20:17:30 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 let addToy = false;
-
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded",function () {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
+  addBtn.addEventListener("click",function () {
+    
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
@@ -13,3 +12,81 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+function createName(toy, card) {
+  let name = document.createElement('h2')
+  name.innerText = toy.name
+  card.appendChild(name)
+}
+
+function createPhoto(toy, card) {
+  let img = document.createElement('img')
+  img.src = toy.image
+  img.className = "toy-avatar"
+  card.appendChild(img)
+}
+
+
+function totalLikes(toy, card) {
+  let likes = document.createElement('p')
+  likes.innerText = `${toy.likes} likes`
+  card.appendChild(likes)
+}
+
+
+function addButton(toy, card) {
+  let newButton = document.createElement('button')
+  newButton.addEventListener('click', function() {
+    increaseCount(toy);
+    window.location.reload(true);
+  })
+  newButton.className = "like-btn"
+  newButton.style = "width: 30px;height:30px;cursor:pointer;"
+  newButton.innerText = "♥"
+  card.appendChild(newButton)
+}
+
+
+
+form = document.querySelector('.add-toy-form')
+form.addEventListener('submit', submitData)
+
+function submitData() {
+
+  let formData = {
+    "name": document.querySelectorAll('.input-text')[0].value,
+    "image": document.querySelectorAll('.input-text')[1].value,
+    "likes": "0"
+  }
+
+  let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+  };
+
+  fetch("http://localhost:3000/toys", configObj)
+      .then(response => response.json())
+      .then(json => console.log(json))
+}
+
+
+function increaseCount(toy) {
+
+  let config = {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          "likes": parseInt(toy.likes) + 1
+        })
+  };
+
+  fetch(`http://localhost:3000/toys/${toy.id}`, config)
+}
